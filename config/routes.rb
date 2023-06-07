cef48897bdeb2484f49e16b9901532c6f423a3da
sbellotlecoq <sbellotlecoq@gmail.com>
2023-06-07 14:08:02 +0200

    premier commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+# api/config/routes
+Rails.application.routes.draw do
+  get 'private/test'
+  devise_for :users, 
+    path: '', 
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+end
